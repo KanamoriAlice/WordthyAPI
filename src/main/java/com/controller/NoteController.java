b5e47cd09d30b34023e71f059977241cda7d4e03
Marcos <megahdez98@hotmail.com>
2023-05-17 17:49:34 -0600

    Cambios necesarios para las notas

--- src/main/java/com/controller/NoteController.java
+++ src/main/java/com/controller/NoteController.java
@@ -30,63 +30,67 @@ import io.swagger.v3.oas.annotations.Operation;
 @RequestMapping("/note")
 @Validated
 public class NoteController {
-	
+
 	@Autowired
 	private NoteService noteService;
-	
+
 	@Operation(summary = "Get all notes reviews")
 	@GetMapping("/allNotesReviews")
 	@ResponseStatus(HttpStatus.OK)
 	public List<NoteDTO> getAllNotesReviews() {
-	    return noteService.getAllNotesReviews();
+		return noteService.getAllNotesReviews();
 	}
 
 	@Operation(summary = "Delete a note by its name")
 	@DeleteMapping("/{name}")
 	@ResponseStatus(HttpStatus.OK)
 	public void delete(
-	        @NotBlank(message = NOTE_NOT_BLANK)
-	        @PathVariable String name) {
-	    noteService.delete(name);
+			@NotBlank(message = NOTE_NOT_BLANK) @PathVariable String name) {
+		noteService.delete(name);
 	}
 
 	@Operation(summary = "Get a note review by its name")
 	@GetMapping("/{name}/noteReview")
 	@ResponseStatus(HttpStatus.OK)
 	public NoteDTO getNoteReview(
-	        @NotBlank(message = NOTE_NOT_BLANK)
-	        @PathVariable String name) {
-	    return noteService.getNoteReview(name);
+			@NotBlank(message = NOTE_NOT_BLANK) @PathVariable String name) {
+		return noteService.getNoteReview(name);
 	}
-	
+
 	@Operation(summary = "Create a new note")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void post(
-	        @NotBlank(message = NOTE_NOT_BLANK)
-	        @RequestParam String name) {
-	    if(noteService.checkIfTitleExists(name))
-	        throw new NameAlreadyExistsException(Note.class,name);
-	    noteService.post(name);
+			@NotBlank(message = NOTE_NOT_BLANK) @RequestParam String name) {
+		if (noteService.checkIfTitleExists(name))
+			throw new NameAlreadyExistsException(Note.class, name);
+		noteService.post(name);
 	}
 
 	@Operation(summary = "Update a note's content by its name")
 	@PatchMapping("/{name}/updateContent")
 	@ResponseStatus(HttpStatus.OK)
 	public void updateContent(
-	        @NotBlank(message = NOTE_NOT_BLANK)
-	        @PathVariable String name,
-	        @RequestParam String content) {
-	    noteService.updateContent(name, content);
+			@NotBlank(message = NOTE_NOT_BLANK) @PathVariable String name,
+			@RequestParam String content) {
+		noteService.updateContent(name, content);
 	}
-	
-//	@PatchMapping("/{name}/rename")
-//	@ResponseStatus(HttpStatus.OK)
-//	public void rename(
-//			@NotBlank(message = NOTE_NOT_BLANK)
-//			@PathVariable String name,
-//			@RequestParam String newName) {
-//		noteService.patch(name, newName);
-//	}
-	
+
+	@Operation(summary = "Get a note by its name")
+	@GetMapping("/{name}/content")
+	@ResponseStatus(HttpStatus.OK)
+	public NoteDTO getNoteDTO(
+			@NotBlank(message = NOTE_NOT_BLANK) @PathVariable String name) {
+		return noteService.getNoteByName(name);
+	}
+
+	// @PatchMapping("/{name}/rename")
+	// @ResponseStatus(HttpStatus.OK)
+	// public void rename(
+	// @NotBlank(message = NOTE_NOT_BLANK)
+	// @PathVariable String name,
+	// @RequestParam String newName) {
+	// noteService.patch(name, newName);
+	// }
+
 }
--- src/main/java/com/service/NoteService.java
+++ src/main/java/com/service/NoteService.java
@@ -23,40 +23,45 @@ public class NoteService {
 	public boolean checkIfTitleExists(String title) {
 		return noteRepository.findByName(title) != null;
 	}
-	
+
 	public void delete(String title) {
 		Note note = noteRepository.findByName(title);
 		noteRepository.delete(note);
 	}
-	
+
 	public List<NoteDTO> getAllNotesReviews() {
 		List<Note> notes = noteRepository.getAllNotesReviews();
 		return notes.stream()
 				.map(note -> mapper.map(note, NoteDTO.class))
 				.collect(Collectors.toList());
 	}
-	
+
 	public NoteDTO getNoteReview(String name) {
 		Note note = noteRepository.getNoteReview(name);
 		return mapper.map(note, NoteDTO.class);
 	}
-	
+
 	public void post(String name) {
 		Note note = new Note(name);
 		noteRepository.save(note);
 	}
-	
+
 	public void rename(String title, String newTitle) {
 		Note note = noteRepository.findByName(title);
 		note.setName(newTitle);
 		noteRepository.save(note);
 	}
-	
+
 	public void updateContent(String title, String content) {
 		Note note = noteRepository.findByName(title);
 		note.setContent(content);
 		note.setLastUpdated(LocalDate.now());
 		noteRepository.save(note);
+	}
+
+	public NoteDTO getNoteByName(String name) {
+		Note note = noteRepository.findByName(name);
+		return mapper.map(note, NoteDTO.class);
 	}
 
 }
